2bac64a29ee8e7798331795598e0d469e6dec6ba
Vincent Wei <codeartisan@outlook.com>
2018-06-11 17:46:48 +0800

    ok

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.getenv('DEBUG') is False else True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
 
 # Application definition
 SITE_ID = 1
